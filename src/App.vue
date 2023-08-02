fa8189fd46da3e9d3a6fe4ac4539b623155fa180
Your Name <oza.abhishek.hr@jp.fujitsu.com>
2023-08-02 18:00:22 +0900

    getting grip

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,184 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="relative-position ">
+    <q-layout view="lHh Lpr lFf" class="relative-position bg-red">
+      <q-header elevated class="glossy">
+        <q-toolbar>
+          <!-- Logo  
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar.png">
+          </q-avatar> -->
+          <q-toolbar-title> physio </q-toolbar-title>
+
+          <div>
+            <q-btn
+              class="menu_bar"
+              flat
+              @click="leftDrawerOpen = !leftDrawerOpen"
+              aria-label="Menu"
+              icon="menu"
+            />
+            <div class="row justify-evenly menu_content">
+              <q-btn flat>About Us</q-btn>
+              <q-btn flat>Our Services</q-btn>
+              <q-btn flat>Contact Us</q-btn>
+            </div>
+          </div>
+        </q-toolbar>
+      </q-header>
+
+      <q-drawer v-model="leftDrawerOpen" bordered class="bg-grey">
+        <q-list style="height: 38%" class="q-mt-xl">
+          <q-item clickable v-ripple class="q-mb-lg">
+            <q-item-section avatar>
+              <q-icon name="groups" class="header_icon" />
+            </q-item-section>
+            <q-item-section class="font_size_header"> About Us </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple class="q-mb-lg">
+            <q-item-section avatar>
+              <q-icon name="psychology" class="header_icon" />
+            </q-item-section>
+            <q-item-section class="font_size_header">
+              Our Services
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="call" class="header_icon" />
+            </q-item-section>
+            <q-item-section class="font_size_header">
+              Contact Us
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-drawer>
+
+      <q-page-container>
+        <HomeView />
+      </q-page-container>
+    </q-layout>
+    <div class="absolute-center q-mt-xl bg-white" style="left: 2%;">
+      <q-icon name="facebook" class="text-h3"/>
+    </div>
+  </div>
 </template>
 
+<script>
+import HomeView from "./views/HomeView.vue";
+import { ref } from "vue";
+export default {
+  name: "LayoutDefault",
+
+  components: {
+    HomeView,
+  },
+
+  setup() {
+    return {
+      leftDrawerOpen: ref(false),
+    };
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.font_size_header {
+  font-size: large;
 }
-
-nav {
-  padding: 30px;
+.header_icon {
+  color: orange;
+  font-size: 30px !important;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+/* --Banner Content Right */
+.banner_content {
+  font-size: 36px;
+  padding: 0 23px;
+  color: white;
 }
+/* --Icon Cover */
+body.screen--xs {
+  /* --Header  */
+  .menu_bar {
+    display: block;
+  }
+  .menu_content {
+    display: none;
+  }
+  /* Header */
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  /* --Info after banner */
+  .info_left {
+    font-size: 16px;
+  }
+}
+body.screen--sm {
+  /* --Header  */
+  .menu_bar {
+    display: block;
+  }
+  .menu_content {
+    display: none;
+  }
+  /* --Header  */
+
+  /* --Info after banner */
+  .info_left {
+    font-size: 16px;
+  }
+}
+body.screen--md {
+  /* --Header  */
+  .menu_bar {
+    display: none;
+  }
+  /* --Header  */
+  /* Banner */
+  .banner_content {
+    font-size: 46px;
+    color: white;
+  }
+  /* Banner */
+
+  /* --Info after banner */
+  .info_left {
+    font-size: 18px;
+  }
+}
+body.screen--lg {
+  /* --Header  */
+  .menu_bar {
+    display: none;
+  }
+  /* --Header  */
+  /* Banner */
+  .banner_content {
+    font-size: 46px;
+    color: white;
+  }
+  /* Banner */
+
+  /* --Info after banner */
+  .info_left {
+    font-size: 20px;
+  }
+}
+body.screen--xl {
+  /* --Header  */
+  .menu_bar {
+    display: none;
+  }
+  /* --Header  */
+  /* Banner */
+  .banner_content {
+    font-size: 46px;
+    color: white;
+  }
+  /* Banner */
+
+  /* --Info after banner */
+  .info_left {
+    font-size: 22px;
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <q-page>
+    <!-- Banner Section -->
+    <div>
+      <q-parallax src="https://cdn.quasar.dev/img/parallax2.jpg" >
+        <div class="custom_width row">
+          <div class="col-12 col-md-5 banner_content flex flex-center">
+            Let Us Help You & Your Family To Maintain The Health
+          </div>
+          <div class="col-md-7 column flex-center gt-sm">
+            <div class="border_card q-mb-md">About Us</div>
+            <div class="border_card q-mb-md">Our Services</div>
+            <div class="border_card">Contact Us</div>
+          </div>
+        </div>
+      </q-parallax>
+    </div>
+
+    <!-- About us -->
+    <div class="container q-mt-md row">
+      <div class="col-12 col-md-6 col-lg-6 flex flex-center q-pa-md info_right">
+        2022 marked 10 years delivering film and television production
+        physiotherapy, osteopathy, on-set massage and health services to
+        productions large and small
+      </div>
+      <div class="col-12 col-md-6 col-lg-6 flex flex-center q-pa-md info_left">
+        The Film Physios exists to keep productions productive. All our services
+        can be delivered on set, on location, on call & in our on the lot
+        clinics.
+        <p></p>
+        We provide physiotherapy/ vocal physiotherapy/ osteopathy/ on-set
+        massage therapy/ transformational PT for talent/ to location clinics/
+        24-7 concierge.
+        <p></p>
+        We operate across studios large and small, from Pinewood, Shepperton,
+        Warner Bros Leavesden, Longcross and Sky Elstree to independent
+        facilities across London and the South east
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+};
 </script>
+<style>
+/* Banner Content */
+.custom_width {
+  height: 300px;
+  width: 100%;
+}
+/* Banner right Content */
+.border_card {
+  width: 560px;
+  text-align: center;
+  border: 1px solid white;
+  padding: 10px 2px;
+  font-size: x-large;
+  font-weight: 600;
+  color: white;
+  cursor: pointer;
+}
+.border_card:hover {
+  background-color: white;
+  color: red;
+  transition: all 0.5s ease-in;
+}
+
+/* Container Common Class */
+.container {
+  width: 80%;
+  margin: 0 auto;
+  height: 355px;
+}
+/* --Info Page Content Management */
+.info_right {
+  font-size: 30px;
+  line-height: 35px;
+}
+
+</style>
